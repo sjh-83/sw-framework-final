452f0ed5844b13e9b18c16b0899389fc43792ec6
sjh-83 <dinohyun0916@naver.com>
2025-06-11 17:13:36 +0900

    -- 학번/ 이름 commit

--- src/main/java/com/swfinal/SwFrameworkFinalApplication.java
+++ src/main/java/com/swfinal/SwFrameworkFinalApplication.java
@@ -8,6 +8,7 @@ public class SwFrameworkFinalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SwFrameworkFinalApplication.class, args);
+		System.out.println("기말고사 프로젝트 생성 성공");
 	}
 
 }
