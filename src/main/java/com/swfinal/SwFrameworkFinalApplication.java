0f5e9f62f2421ada2d188e30f3097ba6658b778f
hiphop9n <hiphop9n@gmail.com>
2025-06-11 10:18:42 +0900

    sw-framework-final-exam

---
+++ src/main/java/com/swfinal/SwFrameworkFinalApplication.java
@@ -0,0 +1,13 @@
+package com.swfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwFrameworkFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwFrameworkFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swfinal/SwFrameworkFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwFrameworkFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
